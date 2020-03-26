64a70a8312f4303ad747fd6df4294244862c5220
O.chemaa <chemaa.omar@lyceeppr.fr>
2020-03-26 13:08:22 +0100

    debut

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(float note 1 float note 2 char prenom)
+{
+	/* code */
+	return 0;
+}
+
+
+void Etudiant::affichage()
+{
+	cout << "prenom:" << prenom << endl;
+	cout << "note 1:" << note 1 << endl;
+	cout << "note 2:"<< note 2 <<endl;
+}
+
+float Etudiant::moyenne()
+float resultat
