c10c4ecc7b9d9b2bfd621ca9e705034b8e39420c
O.chemaa <chemaa.omar@lyceeppr.fr>
2020-03-26 13:07:30 +0100

    debut

---
+++ etudiant.cpp
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <iostream>
+#include "p.h"
+
+using namespace std;
+
+etudiant::etudiant() 
---
+++ etudiant.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class etudiant{
+private:
+
+etudiant(char prenom, float note 1 , float note 2);
+
+
+public:
+	
+void afficher();
+
+void calculerMoyenne();
+
+
+};
+
